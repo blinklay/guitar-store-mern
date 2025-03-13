ce82658b9c237da9a121b917e05527be2d3d67fa
blinklay <danillitvinenkod00@gmail.com>
2025-03-13 20:01:22 +0300

    Create Favorites Page

--- client/src/pages/FavoritesPage.jsx
+++ client/src/pages/FavoritesPage.jsx
@@ -1,3 +1,18 @@
+import Card from "../components/Card/Card";
+import PageTitle from "../components/PageTitle";
+
 export default function FavoritesPage() {
-  return <div>FavoritesPage</div>;
+  return (
+    <>
+      <PageTitle>Избранное</PageTitle>
+
+      <div className="grid grid-cols-5 gap-5 mt-5">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </>
+  );
 }
