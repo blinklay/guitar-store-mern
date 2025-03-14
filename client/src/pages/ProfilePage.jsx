c6c943484eeb4c0745011ee927c295d505dc00e4
blinklay <danillitvinenkod00@gmail.com>
2025-03-14 12:36:57 +0300

    Create Profile Page

--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,3 +1,29 @@
+import Card from "../components/Card/Card";
+import PageTitle from "../components/PageTitle";
+
 export default function ProfilePage() {
-  return <div>ProfilePage</div>;
+  return (
+    <>
+      <PageTitle>Профиль</PageTitle>
+
+      <div className="mt-5 flex items-center gap-5 pb-3 border-[var(--color-main-text)] border-b">
+        <div className="w-[5em] h-[5em] bg-header rounded-full flex items-center justify-center font-bold">
+          ИИ
+        </div>
+
+        <div className="text-xl font-bold border-b border-accent">
+          Иванов Иван
+        </div>
+      </div>
+
+      <div className="mt-5  flex gap-3 items-center">
+        <p className=" text-2xl font-bold">Заказы</p>
+        <p>Всего: 121</p>
+      </div>
+
+      <div className="grid grid-cols-5 mt-5">
+        <Card />
+      </div>
+    </>
+  );
 }
