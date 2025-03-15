3db33a071efe0ad32f875faac66e0a9b87cb7fc7
blinklay <danillitvinenkod00@gmail.com>
2025-03-15 23:06:26 +0500

    Add cart & favorited handlers for Card

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -1,8 +1,24 @@
+import { useState } from "react";
 import { textSeparator } from "../../utils/textSeparator";
 import HeartIcon from "../Icons/HeartIcon";
 import ReactMarkdown from "react-markdown";
 
 export default function Card({ id, title, description, imagePreview, price }) {
+  const [cart, setCart] = useState([]);
+  const [favorites, setFaavorites] = useState([]);
+
+  function handleCart() {
+    if (cart.includes(id))
+      setCart((prev) => prev.filter((item) => item !== id));
+    else setCart((prev) => [...prev, id]);
+  }
+
+  function handleFavorites() {
+    if (favorites.includes(id))
+      setFaavorites((prev) => prev.filter((item) => item !== id));
+    else setFaavorites((prev) => [...prev, id]);
+  }
+
   return (
     <div className="flex flex-col justify-between bg-header rounded-lg overflow-hidden">
       <img
@@ -16,8 +32,8 @@ export default function Card({ id, title, description, imagePreview, price }) {
           <p className="font-medium mt-3 text-xl uppercase">
             {textSeparator(title, 20)}
           </p>
-          <button>
-            <HeartIcon />
+          <button onClick={handleFavorites}>
+            <HeartIcon active={favorites.includes(id)} />
           </button>
         </div>
 
@@ -27,9 +43,21 @@ export default function Card({ id, title, description, imagePreview, price }) {
 
         <div className="flex items-center gap-5 mt-3">
           <p className="text-accent font-medium text-xl">{price} Р</p>
-          <button className="bg-accent py-2 px-3 text-white rounded-md font-medium">
-            В корзину
-          </button>
+          {cart.includes(id) ? (
+            <button
+              onClick={handleCart}
+              className="bg-transparent border border-accent py-2 px-3 text-[var(--color-main-text)] rounded-md font-medium"
+            >
+              Удалить из корзины
+            </button>
+          ) : (
+            <button
+              onClick={handleCart}
+              className="border border-transparent bg-accent py-2 px-3 text-white rounded-md font-medium"
+            >
+              В корзину
+            </button>
+          )}
         </div>
       </div>
     </div>
