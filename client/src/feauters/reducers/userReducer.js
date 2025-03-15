fb97f643da0aff20e6b01f20177af2a22d49d1de
blinklay <danillitvinenkod00@gmail.com>
2025-03-15 22:40:25 +0500

    refactoring user reducer

--- client/src/feauters/reducers/userReducer.js
+++ client/src/feauters/reducers/userReducer.js
@@ -1,24 +1,56 @@
-import { ROLE } from "../../constants";
+import { userTypes } from "../reducerTypes/user.types";
 
 const initialState = {
-  email: null,
-  role: ROLE.GUEST
+  user: null,
+  loading: false,
+  error: null,
+  isAuthenticated: false,
+
+  cartLoading: false,
+  cartError: null,
+
+  favoritesLoading: false,
+  favoritesError: null,
 }
 
 export const userReducer = (state = initialState, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case "SET_USER_EMAIL":
+    case userTypes.FETCH_USER_REQUEST:
+      return { ...state, loading: true, error: null };
+
+    case userTypes.FETCH_USER_SUCCESS:
+      return { ...state, loading: false, items: payload, isAuthenticated: true };
+
+    case userTypes.FETCH_USER_FAILURE:
+      return { ...state, loading: false, error: payload };
+
+    case userTypes.FETCH_CART_REQUEST:
       return {
         ...state,
-        email: payload
+        cartLoading: true,
       }
-    case "SET_USER_ROLE":
+
+    case userTypes.FETCH_CART_SUCCESS:
       return {
         ...state,
-        role: payload
+        cartLoading: false,
+        user: {
+          ...state.user,
+          cart: payload
+        }
       }
+
+    case userTypes.FETCH_CART_FAILURE:
+      return {
+        ...state,
+        cartLoading: false,
+        cartError: payload
+      }
+
+    case userTypes.USER_LOGOUT:
+      return { ...initialState }
     default:
       return state;
   }
