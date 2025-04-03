8928d0718b25b2e7e153491985a166d381de4b3a
blinklay <danillitvinenkod00@gmail.com>
2025-04-03 23:08:21 +0300

    Add pagination

--- server/controllers/admin.controller.js
+++ server/controllers/admin.controller.js
@@ -4,9 +4,19 @@ const UserModel = require("../models/User.model")
 const adminController = {
   async getUsers(req, res) {
     try {
-      const users = await UserModel.find()
+      let { page = 1, limit = 10 } = req.query
+      page = parseInt(page, 10)
+      limit = parseInt(limit, 10)
+      const skip = (page - 1) * limit
+
+      const users = await UserModel.find().skip(skip).limit(limit)
+      const totalUser = await UserModel.countDocuments()
+      const totalPages = Math.ceil(totalUser / limit)
+
       res.status(200).json({
-        users
+        users,
+        totalPages,
+        currentPage: page
       })
     } catch (e) {
       handleError(res, e, "Не удалось получить список пользователей!")
