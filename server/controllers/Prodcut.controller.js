1a78d910bdb255670b8e8a6825c868dcd028967f
blinklay <danillitvinenkod00@gmail.com>
2025-05-22 21:58:10 +0300

    Realize GET current product & by ids array

--- server/controllers/Prodcut.controller.js
+++ server/controllers/Prodcut.controller.js
@@ -26,6 +26,39 @@ const ProcutController = {
         message: "Не удалось получить список товаров!"
       })
     }
+  },
+  async getCurrentProduct(req, res) {
+    try {
+      const { productId } = req.params
+      const product = await ProductModel.findById(productId)
+
+      if (!product) {
+        return res.status(404).json({ message: "Товар не найден!" })
+      }
+
+      res.status(200).json({ product })
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Не удалось получить товар!" })
+    }
+  },
+  async getProdcutsByIds(req, res) {
+    try {
+      const { ids } = req.body
+
+      if (!Array.isArray(ids) || ids.length === 0) {
+        return res.status(400).json({ message: "Неправильный формат массива товаров или массив пуст!" })
+      }
+
+      const products = await ProductModel.find({
+        _id: { $in: ids }
+      })
+
+      res.status(200).json({ products })
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ message: "Не удалось получить товары!" })
+    }
   }
 }
 
--- server/routes/Product.router.js
+++ server/routes/Product.router.js
@@ -3,5 +3,7 @@ const ProcutController = require("../controllers/Prodcut.controller")
 const ProductRouter = express.Router()
 
 ProductRouter.get("/", ProcutController.getAllProdcuts)
+ProductRouter.get("/current/:productId", ProcutController.getCurrentProduct)
+ProductRouter.get("/byIds", ProcutController.getProdcutsByIds)
 
 module.exports = ProductRouter
